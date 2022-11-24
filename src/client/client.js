7dfc8b36cf0f0b2f4f558b8e5f8410a20c4dc1ab
mattscript <ayobamifagoroye@yahoo.com>
2022-11-24 23:47:16 +0100

    second commit

--- src/client/client.js
+++ src/client/client.js
@@ -17,7 +17,7 @@ export const GET_STATUS = gql`
 `
 
 export  const client = new ApolloClient({
-    uri: 'http://localhost:4000',
+    uri:  'https://jealous-deer-suspenders.cyclic.app',
     connectToDevTools:true,
     cache: new InMemoryCache({
       typePolicies:{
